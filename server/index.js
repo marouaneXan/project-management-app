34f175ec077e20c214dd7c13241d1495e91cc328
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-14 14:40:01 +0100

    create simple data for project and client

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const PORT = process.env.PORT || 5000
-app.listen(PORT ,()=>console.log('Server running...'))
+const express = require("express");
+require("dotenv").config();
+const { graphQlHTTP } = require("express-graphql");
+const schema =require('./schema/schema')
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/graphql',graphQlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV==='development',
+}))
+
+
+
+app.listen(PORT, () => console.log("Server running..."));
